d800223cfe8c87bdc623e7c66d3bbc408dc52af8
Weekend Architect <32bitengineer@gmail.com>
2026-03-14 20:59:09 +0530

    Initial Commit

---
+++ unift/src/main/java/com/weekend/architect/unift/UniFtApplication.java
@@ -0,0 +1,13 @@
+package com.weekend.architect.unift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniFtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniFtApplication.class, args);
+	}
+
+}
